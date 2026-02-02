bbfdb236a3c310d46fd9558147cd3e5b6ae1bd08
Daniel <s10258472@connect.np.edu.sg>
2026-02-02 21:09:10 +0800

    added streaks helper first (in preparation for streaks feature)

---
+++ app/src/main/java/np/ict/mad/studybuddy/feature/streaks/StreaksFirestore.kt
@@ -0,0 +1,66 @@
+package np.ict.mad.studybuddy.feature.streaks
+
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import java.time.LocalDate
+import java.time.ZoneId
+
+class StreaksFirestore {
+
+    private val db = FirebaseFirestore.getInstance() // the reference to firebase db
+
+    // will return today's date as a string (using sg timestone)
+    private fun todayString(): String{
+        val sg = ZoneId.of("Asia/Singapore")
+        return LocalDate.now(sg).toString()
+    }
+
+    // return yesterday's date as a string - used to see if the streak continues
+    private fun yesterdayString(): String{
+        val sg = ZoneId.of("Asia/Singapore")
+        return LocalDate.now(sg).minusDays(1).toString()
+    }
+
+    // record the study activity for the user
+    suspend fun recordStudyActivity(uid: String){
+        // reference to the user document on firebase
+        val userRef = db.collection("users").document(uid)
+
+        val today = todayString() //todaydate
+        val yesterday = yesterdayString() //yesterdaydate
+
+        db.runTransaction { tx ->
+            val snap = tx.get(userRef)
+
+            // reads the existing streak data (if no data, sets the default values)
+            val lastDate = snap.getString("lastStudyDate") ?: ""
+            val currentStreak = (snap.getLong("studyStreak") ?: 0L).toInt()
+            val longestStreak = (snap.getLong("longestStreak") ?: 0L).toInt()
+
+            // determines the new streak (same day reflection submitted;unchanged,consecutive submission;change streak)
+            val newStreak = when {
+                lastDate == today -> currentStreak
+                lastDate == yesterday -> currentStreak + 1
+                else -> 0
+            }
+
+            // merging streak data to the user document on firebase
+            tx.set(
+                userRef,
+                mapOf(
+                    "studyStreak" to newStreak,
+                    "lastStudyDate" to today,
+                ),
+                com.google.firebase.firestore.SetOptions.merge()
+            )
+
+            null
+        }.await()
+    }
+
+    // retrieves current streak from user to display in home screen
+    suspend fun getStreak(uid: String): Int {
+        val snap = db.collection("users").document(uid).get().await()
+        return (snap.getLong("studyStreak") ?: 0L).toInt()
+    }
+}
